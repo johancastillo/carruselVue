47705429a0fd349147b4bbf869890eec6aef8a2f
Johan Castillo <jcjohan2707@gmail.com>
2020-09-08 11:33:12 -0400

    Carrusel funcional

--- components/carrusel.js
+++ components/carrusel.js
@@ -1,8 +1,165 @@
 Vue.component('carrusel', {
   template: `
-  <div>
-    <h1>Hola Mundo</h1>
-  </div>
+  <div class="slick-list" id="slick-list">
+            <button class="slick-arrow slick-prev" id="button-prev">
+                <i class="fas fa-chevron-left"></i>
+            </button>
+            <div class="slick-track" id="track">
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/1.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/2.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/3.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/4.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/5.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/6.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/7.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/8.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/9.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/10.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/11.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/12.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/13.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/14.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+                <div class="slick">
+                    <div>
+                        <a href="/">
+                            <h4><small>Share Your Message</small>Watch</h4>
+                            <picture>
+                                <img src="./images/15.jpg" alt="Image">
+                            </picture>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <button class="slick-arrow slick-next" id="button-next">
+                <i class="fas fa-chevron-right"></i>
+            </button>
+        </div>
   `,
   data(){
     return {
